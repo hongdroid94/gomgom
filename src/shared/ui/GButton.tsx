0d84581604b59cd54b9ad97b11460d42fd2e9510
devaspirant0510 <seungho020510@gmail.com>
2025-03-03 18:12:32 +0900

    feat: shared 버튼 아이콘 설정

--- src/shared/ui/GButton.tsx
+++ src/shared/ui/GButton.tsx
@@ -8,11 +8,14 @@ type ButtonProps = {
     children?: React.ReactNode;
     onClick?: () => void;
     loading?: boolean;
+    icon?:string,
+    iconPos?:'left'|'bottom'|'right'|'top',
+
 };
 
 // 공통 위젯
 // TODO : 디자인 확정 나면 컴포넌트 스타일 적용
-const GButton: FC<ButtonProps> = ({ className, children, onClick,loading }) => {
+const GButton: FC<ButtonProps> = ({ className, children, onClick,loading,icon,iconPos }) => {
     return (
         <Button
             loading={loading}
@@ -20,6 +23,8 @@ const GButton: FC<ButtonProps> = ({ className, children, onClick,loading }) => {
             label={children?.toString()}
             className={clsx(className)}
             onClick={onClick}
+            icon={icon}
+            iconPos={iconPos}
         ></Button>
     );
 };
